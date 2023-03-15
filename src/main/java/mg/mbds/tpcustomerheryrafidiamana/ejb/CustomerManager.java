73230f3b5ce49c2fdbe609a3e7812c080ea1cb45
Hery Rafidimanana <heryrafidimanana@gmail.com>
2023-03-15 21:10:23 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/mbds/tpcustomerheryrafidiamana/ejb/CustomerManager.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.mbds.tpcustomerheryrafidiamana.ejb;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+import mg.mbds.tpcustomerheryrafidiamana.entities.Customer;
+
+/**
+ *
+ * @author Hery Andrianirina
+ */
+@Stateless
+public class CustomerManager {
+    
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {  
+        Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }  
+        
+    public Customer update(Customer customer) {
+      return em.merge(customer);
+    }  
+    
+    public void persist(Customer customer) {
+      em.persist(customer);
+    }
+}
